e590d60de989e470eed47d7bbcadd34af03619f2
querenneves <querensantos1@hotmail.com>
2023-10-23 18:38:16 -0300

    Deploy do projeto loja games

--- src/main/java/com/generation/games/model/Usuario.java
+++ src/main/java/com/generation/games/model/Usuario.java
@@ -2,6 +2,7 @@ package com.generation.games.model;
 
 import java.time.LocalDate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +24,7 @@ public class Usuario {
 	@NotNull(message = "O Atributo Nome é Obrigatório!")
 	private String nome;
 
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O Atributo Usuário é Obrigatório!")
 	@Email(message = "O Atributo Usuário deve ser um email válido!")
 	private String usuario;
